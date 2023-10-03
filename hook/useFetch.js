a24b6587d6eb4eecf60ef547b216d9fde66e5334
Bimsara Rathnayake <bimsararath67@gmail.com>
2023-10-03 12:26:03 +0530

    react native job complete

---
+++ hook/useFetch.js
@@ -0,0 +1,51 @@
+import { useRootNavigationState, useEffect, useState } from "react";
+import axios from "axios";
+// import { RAPID_API_KEY } from "@env";
+// import { resolveHref } from "expo-router/build/link/href";
+
+// const rapidApiKey = RAPID_API_KEY;
+
+const useFetch = (endpoint, query) => {
+  const [data, setData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
+  const [error, setError] = useState(null);
+
+  const options = {
+    method: "GET",
+    url: `https://jsearch.p.rapidapi.com/${endpoint}`,
+    headers: {
+      "X-RapidAPI-Key": "8a4eaf9d3fmsh9f86e45b0db4a95p174b47jsn53ff3be3a094",
+      "X-RapidAPI-Host": "jsearch.p.rapidapi.com",
+    },
+    params: { ...query },
+  };
+
+  const fetchData = async () => {
+    setIsLoading(true);
+
+    try {
+      const response = await axios.request(options);
+
+      setData(response.data.data);
+      setIsLoading(false);
+    } catch (error) {
+      setError(error);
+      alert("There is an error");
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
+  const refetch = () => {
+    setIsLoading(true);
+    fetchData();
+  };
+
+  return { data, isLoading, error, refetch };
+}
+
+export default useFetch;
